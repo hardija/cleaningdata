5709cb91a0f61c87933d77e36b0dfb379be3012b
Jeff <jeff@jeffreys-mbp.home.net>
2014-04-25 06:25:37 -0400

    Initial Commit

---
+++ run_analysis.R
@@ -0,0 +1,79 @@
+library(reshape2)
+
+# For testing set maxrows = 20, set maxrows = -1 for all
+#maxrows = 20
+maxrows = -1
+
+# Find the columns for std and mean
+features = read.csv("./data/UCI HAR Dataset/features.txt", sep=" ", header=FALSE)
+features = features[,2]             # Just keep the feature names
+std = grep("std", features)         # Find the column indices for std dev
+mean = grep("mean", features)       # Find the column indices for mean
+# Clean up the feature names
+features = gsub("-", "_", features)
+features = gsub("\\(", "", features)
+features = gsub("\\)", "", features)
+features = gsub(",", "_", features)
+featureCols = sort(c(std,mean))     # Combine std and mean columns and sort
+numFeatures = length(features)      # Remember the number of features (used below)
+
+# Clean up memory
+if (maxrows == -1) rm(list=c("std","mean"))
+
+# Read X Train data and provide column names
+x_train = read.fwf("./data/UCI HAR Dataset/train/X_train.txt", rep(16, numFeatures), col.names=features, n=maxrows)
+# Subset on mean and std dev
+x_train = x_train[,featureCols]
+# Add subject information
+subject_train = read.fwf("./data/UCI HAR Dataset/train/subject_train.txt", c(5), col.names=c("Subject"), n=maxrows)
+x_train = cbind(subject_train, x_train)
+# Clean up memory
+if (maxrows == -1) rm("subject_train")
+
+# Read X Test data and provide column names
+x_test = read.fwf("./data/UCI HAR Dataset/test/X_test.txt", rep(16, numFeatures), col.names=features, n=maxrows)
+# Subset on mean and std dev
+x_test = x_test[,featureCols]
+# Add subject information
+subject_test = read.fwf("./data/UCI HAR Dataset/test/subject_test.txt", c(5), col.names=c("Subject"), n=maxrows)
+x_test = cbind(subject_test, x_test)
+# Clean up memory
+if (maxrows == -1) rm("subject_test")
+
+# Combine train and test rows
+x_train = rbind(x_train, x_test)
+# Clean up memory
+if (maxrows == -1) rm("x_test")
+
+# Read Y Train data and provide column names
+y_train = read.fwf("./data/UCI HAR Dataset/train/y_train.txt", c(5), col.names=c("Activity"), n=maxrows)
+# Read Y Test data and provide column names
+y_test = read.fwf("./data/UCI HAR Dataset/test/y_test.txt", c(5), col.names=c("Activity"), n=maxrows)
+
+# Combine train and test rows
+y_train = rbind(y_train, y_test)
+# Clean up memory
+if (maxrows == -1) rm("y_test")
+
+# Add factors for activity names
+activities = read.fwf("./data/UCI HAR Dataset/activity_labels.txt", c(2, 18), col.names=c("id", "Activity"))
+y_train[,1] = cut(y_train[,1], c(0,activities[,1]), labels=activities[,2])
+
+# Combine x and y columns
+data = cbind(x_train, y_train)
+# Clean up memory
+if (maxrows == -1) rm(list=c("x_train", "y_train", "activities"))
+
+# Write out the data set (not required)
+# Don't include row names or the column names get shifted
+#write.table(data, file="./data/output.csv", sep=",", row.names=FALSE)
+
+# Create a tidy dataset with the average of each variable for each activity and each subject
+molten = melt(data, id=c("Subject","Activity"))
+tidy = dcast(molten, Subject + Activity ~ variable, mean)
+# Clean up memory
+if (maxrows == -1) rm(list=c("data", "molten"))
+
+write.table(tidy, file="./data/tidy.csv", sep=',', row.names=FALSE)
+
+write.table(colnames(tidy), file="./data/CodeBook.txt", sep=' ', row.names=FALSE)
