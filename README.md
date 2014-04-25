780243489b6b6f51736cc2f37e447b621017affb
Jeff <jeff@Jeffreys-MacBook-Pro.local>
2014-04-25 11:55:08 -0400

    Updated README, final tidy

--- README.md
+++ README.md
@@ -1,29 +1,65 @@
+## Getting and Cleaning Data Project
+###Overview
+This document describes the data and the procedure used for this project.
+More detailed information can be found in the comments of the code.
+
 ###Input Data:
-*subjects = 30
-*features = 561
-*train data = 7352 obs, 128 vars
-*test data = 2947 obs, 128 vars
-*x_train = 7352 obs, 561 vars
-*y_train = 7352 obs, 1 var
-*x_test = 2947 obs, 561 vars
-*y_test = 2947 obs, 1 var
+The input data consist of 561 measurements for 30 subjects each doing 6 different activities.
+The data are broken into two sets originally intended for machine learning; a training set and a test set, each with x inputs and y outputs.
 
-Procedure:
-1. get feature names from features.txt
-2. get feature subset for mean and std dev
-3. read x_train file and provide column names
-4. subset x_train for std and mean only
-5. read subject_train file and provide column name
-6. add subject column to x_train => cbind
-7. read x_test file and provide column names
-8. subset x_test for std and mean only
-9. read subject_test file and provide column name
-10. add subject column to x_test => cbind
-11. merge x_train rows with x_test rows using rbind
-12. read y_train and y_test files and provide column names
-13. merge y_train rows with y_test rows
-14. add factors for y
-15. merge x columns with y column => cbind
+The data are summarized as follows:
 
-Output data:
+* features = 561 obs, 2 var - Contains a feature ID and feature description for 561 measurements
+* X\_train = 7352 obs, 561 vars - Contains the measurements for each feature
+* X\_test = 2947 obs, 561 vars - Contains the measurements for each feature
+* y\_train = 7352 obs, 1 var - Contains the activity number for each observation in X\_train
+* y\_test = 2947 obs, 1 var - Contains the activity number for each observation in X\_test
+* subject\_train = 7352 obs, 1 var - Contains the subject number for each observation in X\_train
+* subject\_test = 2947 obs, 1 var - Contains the subject number for each observation in X\_test
+* activities = 6 obs, 2 var - Contains names of the activities for each activity number
 
+Detailed information on how the measurements were made and what the measurements represent can be found in features\_info.txt in the input dataset folder.
+
+###Goal
+The goal of the project is to find all variables that represent mean or standard deviation values and produce a dataset that averages these values by subject and activity.
+
+There are 79 variables that have mean, std or meanFreq in the name. These were selected for analysis.
+
+* train data = 7352 obs, 79 vars
+* test data = 2947 obs, 79 vars
+
+The final data is a table of the mean of each variable by subject and activity. This results in a table of 180 rows (30 subjects * 6 activities) and 81 columns (subject, activity and 79 measurements).
+
+The final data are written to tidy.csv.
+
+###Procedure:
+####Setup
+For the script to run, the input data should be extracted to the same directory as run\_analysis.R. In other words, you should have the folder "UCI HAR Dataset" and run\_analysis.R in the same working directory.
+
+The input data can be found here:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+####Analysis
+The following describes the procedure used in run_analysis.R to manipulate the data and produce a tidy output dataset:
+
+1. Get feature names by reading features.txt and subsetting just the names column
+2. Get feature subset for mean and std dev using grep and sorting an array of column indices
+3. Read X\_train data from X\_train.txt and provide column names from features data
+4. Subset X\_train for std and mean only
+5. Read subject\_train data from subject\_train.txt and provide column name
+6. Add subject column to X\_train using cbind
+7. Read X\_test data from X\_test.txt and provide column names from features data
+8. Subset X\_test for std and mean only
+9. Read subject\_test data from subject\_test.txt and provide column name
+10. Add subject column to X\_test using cbind
+11. Add X\_test rows to X\_train using rbind
+12. Read y\_train and y\_test files and provide column names
+13. Add y\_test rows to y\_train using rbind
+14. Add factors for y\_train activity names using cut
+15. Merge X columns with y column using cbind
+16. Create tidy dataset with the mean of each measurement for each subject and activity using melt and dcast
+17. Write tidy dataset to file using write.table
+
+####Notes:
+* Since the datasets are large, memory was released when it was no longer needed. This would allow the script to scale better if even larger datasets were used.
+* A variable, maxrows, was used to reduce the amount of data read in for testing. This sped up the testing phase.
--- run_analysis.R
+++ run_analysis.R
@@ -5,50 +5,50 @@ library(reshape2)
 maxrows = -1
 
 # Find the columns for std and mean
-features = read.csv("./data/UCI HAR Dataset/features.txt", sep=" ", header=FALSE)
+features = read.csv("./UCI HAR Dataset/features.txt", sep=" ", header=FALSE)
 features = features[,2]             # Just keep the feature names
-std = grep("std", features)         # Find the column indices for std dev
-mean = grep("mean", features)       # Find the column indices for mean
+std_idx = grep("std", features)         # Find the column indices for std dev
+mean_idx = grep("mean", features)       # Find the column indices for mean
 # Clean up the feature names
 features = gsub("-", "_", features)
 features = gsub("\\(", "", features)
 features = gsub("\\)", "", features)
 features = gsub(",", "_", features)
-featureCols = sort(c(std,mean))     # Combine std and mean columns and sort
+featureCols = sort(c(std_idx, mean_idx))     # Combine std and mean columns and sort
 numFeatures = length(features)      # Remember the number of features (used below)
 
 # Clean up memory
-if (maxrows == -1) rm(list=c("std","mean"))
+if (maxrows == -1) rm(list=c("std_idx", "mean_idx"))
 
-# Read X Train data and provide column names
-x_train = read.fwf("./data/UCI HAR Dataset/train/X_train.txt", rep(16, numFeatures), col.names=features, n=maxrows)
+# Read X train data and provide column names
+X_train = read.fwf("./UCI HAR Dataset/train/X_train.txt", rep(16, numFeatures), col.names=features, n=maxrows)
 # Subset on mean and std dev
-x_train = x_train[,featureCols]
+X_train = X_train[,featureCols]
 # Add subject information
-subject_train = read.fwf("./data/UCI HAR Dataset/train/subject_train.txt", c(5), col.names=c("Subject"), n=maxrows)
-x_train = cbind(subject_train, x_train)
+subject_train = read.fwf("./UCI HAR Dataset/train/subject_train.txt", c(5), col.names=c("Subject"), n=maxrows)
+X_train = cbind(subject_train, X_train)
 # Clean up memory
 if (maxrows == -1) rm("subject_train")
 
-# Read X Test data and provide column names
-x_test = read.fwf("./data/UCI HAR Dataset/test/X_test.txt", rep(16, numFeatures), col.names=features, n=maxrows)
+# Read X test data and provide column names
+X_test = read.fwf("./UCI HAR Dataset/test/X_test.txt", rep(16, numFeatures), col.names=features, n=maxrows)
 # Subset on mean and std dev
-x_test = x_test[,featureCols]
+X_test = X_test[,featureCols]
 # Add subject information
-subject_test = read.fwf("./data/UCI HAR Dataset/test/subject_test.txt", c(5), col.names=c("Subject"), n=maxrows)
-x_test = cbind(subject_test, x_test)
+subject_test = read.fwf("./UCI HAR Dataset/test/subject_test.txt", c(5), col.names=c("Subject"), n=maxrows)
+X_test = cbind(subject_test, X_test)
 # Clean up memory
-if (maxrows == -1) rm("subject_test")
+if (maxrows == -1) rm(list=c("subject_test", "features", "featureCols"))
 
 # Combine train and test rows
-x_train = rbind(x_train, x_test)
+X_train = rbind(X_train, X_test)
 # Clean up memory
-if (maxrows == -1) rm("x_test")
+if (maxrows == -1) rm("X_test")
 
-# Read Y Train data and provide column names
-y_train = read.fwf("./data/UCI HAR Dataset/train/y_train.txt", c(5), col.names=c("Activity"), n=maxrows)
-# Read Y Test data and provide column names
-y_test = read.fwf("./data/UCI HAR Dataset/test/y_test.txt", c(5), col.names=c("Activity"), n=maxrows)
+# Read y train data and provide column names
+y_train = read.fwf("./UCI HAR Dataset/train/y_train.txt", c(5), col.names=c("Activity"), n=maxrows)
+# Read y test data and provide column names
+y_test = read.fwf("./UCI HAR Dataset/test/y_test.txt", c(5), col.names=c("Activity"), n=maxrows)
 
 # Combine train and test rows
 y_train = rbind(y_train, y_test)
@@ -56,17 +56,17 @@ y_train = rbind(y_train, y_test)
 if (maxrows == -1) rm("y_test")
 
 # Add factors for activity names
-activities = read.fwf("./data/UCI HAR Dataset/activity_labels.txt", c(2, 18), col.names=c("id", "Activity"))
+activities = read.fwf("./UCI HAR Dataset/activity_labels.txt", c(2, 18), col.names=c("id", "Activity"))
 y_train[,1] = cut(y_train[,1], c(0,activities[,1]), labels=activities[,2])
 
-# Combine x and y columns
-data = cbind(x_train, y_train)
+# Combine X and y columns
+data = cbind(X_train, y_train)
 # Clean up memory
-if (maxrows == -1) rm(list=c("x_train", "y_train", "activities"))
+if (maxrows == -1) rm(list=c("X_train", "y_train", "activities"))
 
 # Write out the data set (not required)
 # Don't include row names or the column names get shifted
-#write.table(data, file="./data/output.csv", sep=",", row.names=FALSE)
+#write.table(data, file="./output.csv", sep=",", row.names=FALSE)
 
 # Create a tidy dataset with the average of each variable for each activity and each subject
 molten = melt(data, id=c("Subject","Activity"))
@@ -74,6 +74,4 @@ tidy = dcast(molten, Subject + Activity ~ variable, mean)
 # Clean up memory
 if (maxrows == -1) rm(list=c("data", "molten"))
 
-write.table(tidy, file="./data/tidy.csv", sep=',', row.names=FALSE)
-
-write.table(colnames(tidy), file="./data/CodeBook.txt", sep=' ', row.names=FALSE)
+write.table(tidy, file="./tidy.csv", sep=',', row.names=FALSE)
